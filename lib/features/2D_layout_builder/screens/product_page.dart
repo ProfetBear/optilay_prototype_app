5a74e175c1e5e7a9612dc91a1ed99816d6f5543e
ProfetBear <matteo.maggi@outlook.com>
2026-03-09 20:23:19 +0100

    changed file name

--- lib/features/2D_layout_builder/screens/product_page.dart
+++ lib/features/2D_layout_builder/screens/product_page.dart
@@ -1,7 +1,7 @@
 // lib/features/product/product_page.dart
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:optilay_prototype_app/features/3D_model_viewer/model_viewer_embed.dart';
+import 'package:optilay_prototype_app/features/3D_model_viewer/3D_model_viewer_embed.dart';
 import 'package:optilay_prototype_app/routes/routes.dart';
 import 'package:optilay_prototype_app/utils/constants/colors.dart';
 
